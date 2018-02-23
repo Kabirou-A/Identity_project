a0894f4644188ea73cf2e848f6c4a6ad07f8ad71
karinloye <adebowaledocallprogram@gmail.com>
2018-02-23 10:45:55 +0100

    I make few changes and comment some of public methods uncomment.

--- src/fr/epita/iam/services/MenuService.java
+++ src/fr/epita/iam/services/MenuService.java
@@ -16,7 +16,7 @@ import fr.epita.logger.Logger;
  * @author ADEBOWALE
  * MenuService will display on screen and allow register user to perform CRUD operation
  */
-public class MenuService {
+public class MenuService extends DisplayMenu{
 	private static final Logger LOGGER = new Logger(MenuService.class);
 	
 
@@ -24,31 +24,20 @@ public class MenuService {
 	 * Default construction to create instance of MenuService object  
 	 */
 	public MenuService() {
-		
+		new DisplayMenu();
 	}
 	
 	/*
 	 * create the main menu to perform CRUD operations
 	 */
-	public void mainMenu() {
-		System.out.println("Welcome to IdentityApp !!!!!");
-		System.out.println("Select : ");
-		System.out.println("0 - to Exit IdentityApp");
-		System.out.println("1 - to perform CRUD operations on  IDENTITIES table");
-		System.out.println("2 - to perform CRUD operations on  ADDRESS table");	
-	}
+	
 	
 	/*
 	 * IdentityMenu() method to perform CRUD operations on  IDENTITIES table
 	 */
 	public void IdentityMenu() {
-		System.out.println("Select : ");
-		System.out.println("C - to create an Identity");
-		System.out.println("R - to search an Identity");
-		System.out.println("U - to update an Identity");	
-		System.out.println("D - to delete an Identity");
-		System.out.println("3 - to getback to main menu");
-		System.out.println("0 - to Exit IdentityApp");
+		
+		identityMenu();
 		
 		Scanner keyboard = new Scanner( System.in );
 		String input = keyboard.nextLine().toLowerCase();
@@ -60,62 +49,20 @@ public class MenuService {
 	            break;
 	         
 	         case "c": 
-	        	 Identity identityC = new Identity();
-	        	 System.out.println("Input Identity name");
-	        	 identityC.setDisplayName(keyboard.nextLine());
-	        	 System.out.println("Input Identity email");
-	        	 identityC.setEmail(keyboard.nextLine());
-	        	 System.out.println("Input Identity user id");
-	        	 identityC.setUid(keyboard.nextLine());
-	        	 	        	 
-	        	 try {
-					new IdentityJDBCDAO().create(identityC);
-				} catch (IdentityCreationException e) {
-					e.printStackTrace();
-				}
-	        	IdentityMenu();
-	            break;
-	            
+	        	 consoleCreateIdentity();
+	        	 IdentityMenu();
+	             break;
 	         case "r" : 
-	        	 Identity identityR = new Identity();
-	        	 System.out.println("Input Identity details you are searching (display or email or uid)!!!");
-	        	 identityR.setDisplayName(keyboard.nextLine());
-	        	 
-	        	 try {
-						new IdentityJDBCDAO().search(identityR);
-					} catch (IdentitySearchException e) {
-						e.printStackTrace();
-					
-					}
+	        	 consoleSearchIdentity();
 	        	 IdentityMenu();
 	         	break;
 	         
 	         case "u" :
-	        	 Identity identityU = new Identity();
-	        	 System.out.println("Input the Identity name you want to update");
-	        	 identityU.setDisplayName(keyboard.nextLine());
-	        	 System.out.println("Input the Identity email you want to update");
-	        	 identityU.setEmail(keyboard.nextLine());
-	        	 System.out.println("Input the Identity user id you want to update");
-	        	 identityU.setUid(keyboard.nextLine());
-	        	 try {
-						new IdentityJDBCDAO().update(identityU);
-					} catch (IdentityUpdateException e) {
-						e.printStackTrace();
-					
-					}
+	        	 consoleUpdateIdentity();
 	        	 IdentityMenu();
 	         	break;
 	         case "d" : 
-	        	 Identity identityD = new Identity();
-	        	 System.out.println("Input the Identity User ID you want to delete!!!");
-	        	 identityD.setUid(keyboard.nextLine());
-	        	 try {
-						new IdentityJDBCDAO().delete(identityD);
-					} catch (IdentityDeleteException e) {
-						e.printStackTrace();
-					
-					}
+	        	 consoleDeleteIdentity();
 	        	 IdentityMenu();
 	         	break;
 	         	
@@ -134,14 +81,8 @@ public class MenuService {
 	 * AddressMenu() method to perform CRUD operations on  ADDRESS table
 	 */
 	public void AddressMenu() {
-		System.out.println("Select : ");
-		System.out.println("C - to create an Address");
-		System.out.println("R - to search an Address");
-		System.out.println("U - to update an Address");	
-		System.out.println("D - to delete an Address");
-		System.out.println("3 - to getback to main menu");
-		System.out.println("0 - to Exit IdentityApp");
-		
+			
+		addressMenu();
 		
 		Scanner keyboard = new Scanner( System.in );
 		String input = keyboard.nextLine().toLowerCase();
@@ -149,7 +90,8 @@ public class MenuService {
 		while(input != "0"){
 			
 			 switch(input){
-	         case "0" : System.out.println("You close the App"); System.exit(0);
+	         case "0" : System.out.println("You close the App"); 
+	         			System.exit(0);
 	            break;
 	         
 	         case "c": 
@@ -176,7 +118,8 @@ public class MenuService {
 	         case "3" : mainMenuOption();
 	         	break;
 	            
-	         default :System.out.println("Invalid selection"); mainMenuOption();
+	         default :	System.out.println("Invalid selection"); 
+	         			mainMenuOption();
 	            break;	
 			 }
 		 break;
@@ -189,13 +132,14 @@ public class MenuService {
 	public void mainMenuOption() {
 		Scanner keyboard = new Scanner( System.in );
 		
-		mainMenu();
+		firstMenu();
 		
 		String input = keyboard.nextLine();
 		while(input != "0"){
 			
 			 switch(input){
-	         case "0" : System.out.println("You close the App"); System.exit(0);
+	         case "0" : System.out.println("You close the App"); 
+	         			System.exit(0);
 	            break;
 	         
 	         case "1" : IdentityMenu();
@@ -204,7 +148,8 @@ public class MenuService {
 	         case "2" : AddressMenu();
 	         	break;
 	            
-	         default :System.out.println("Invalid selection"); mainMenuOption();
+	         default :	System.out.println("Invalid selection"); 
+	         			mainMenuOption();
 	            break;	
 	            
 			 }
